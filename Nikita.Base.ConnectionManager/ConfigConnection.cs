3950a2ed327b42356ce120e6715dd852b87de6ad
lhmlyx2723356 <871939149@qq.com>
2016-09-17 12:18:09 +0800

    Update Code

--- Nikita.Base.ConnectionManager/ConfigConnection.cs
+++ Nikita.Base.ConnectionManager/ConfigConnection.cs
@@ -7,16 +7,14 @@ namespace Nikita.Base.ConnectionManager
 {
     public static class ConfigConnection
     {
-        public static string NotePadConnection = "server= UKYNDA-001 ;uid=sa;pwd=12345678;database=Note";
-        public static string BugCloseConnection = "server= UKYNDA-001 ;uid=sa;pwd=12345678;database=BugClose";
-        public static string CodeMgrDemoConnection = "server= UKYNDA-001 ;uid=sa;pwd=12345678;database=CodeMgr";
-        public static string BPMDemoConnection = 
-        "server= UKYNDA-001 ;uid=sa;pwd=12345678;database=BPM";
-        public  static  string TestConnection= "server= UKYNDA-001 ;uid=sa;pwd=12345678;database=master";
-
-        public static string WcfConfigDbConnection = @"server= UKYNDA-001;uid=sa;pwd=12345678;database=WcfConfig";
-        public static string CacheStoreConnection = @"server= UKYNDA-001 ;uid=sa;pwd=12345678;database=CacheStore";
-        public static string IDBuilderConnection = "Data Source=UKYNDA-001\\SQLSERVER2008;Initial Catalog=IDBuilder;Persist Security Info=True;User ID=sa;Password=12345678";
+        public static string NotePadConnection = "server= localhost ;uid=sa;pwd=12345678;database=Note";
+        public static string BugCloseConnection = "server= localhost ;uid=sa;pwd=12345678;database=BugClose";
+        public static string CodeMgrDemoConnection = "server= localhost ;uid=sa;pwd=12345678;database=CodeMgr";
+        public static string BPMDemoConnection =  "server= localhost ;uid=sa;pwd=12345678;database=BPM";
+        public  static  string TestConnection= "server= localhost ;uid=sa;pwd=12345678;database=master"; 
+        public static string WcfConfigDbConnection = @"server= localhost;uid=sa;pwd=12345678;database=WcfConfig";
+        public static string CacheStoreConnection = @"server= localhost ;uid=sa;pwd=12345678;database=CacheStore";
+        public static string IDBuilderConnection = "Data Source=localhost;Initial Catalog=IDBuilder;Persist Security Info=True;User ID=sa;Password=12345678";
 
     }
 }
